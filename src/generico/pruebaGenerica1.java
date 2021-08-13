864285faf69e08447a3c0f044b117026ed45ddca
git23user <pepejoseblog@gmail.com>
2021-08-13 16:50:43 +0200

    Commit de 2 proyectos

---
+++ src/generico/pruebaGenerica1.java
@@ -0,0 +1,12 @@
+package generico;
+
+public class pruebaGenerica1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hola mundo... generico1");
+
+	}
+
+}
---
+++ src/generico2/generico2.java
@@ -0,0 +1,10 @@
+package generico2;
+
+public class generico2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola mundo... generico2");
+	}
+
+}
