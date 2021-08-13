96ccdedbac20c0abe17010ab027ff69036ba4d71
git23user <pepejoseblog@gmail.com>
2021-08-13 16:57:26 +0200

    Commit desde ejemplo 3

---
+++ src/Ejercicio3/Ejemplo3.java
@@ -0,0 +1,15 @@
+package Ejercicio3;
+
+public class Ejemplo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hola mundo... Desde Ejercicio nº 3");
+		int j;
+		float p;
+
+
+	}
+
+}
